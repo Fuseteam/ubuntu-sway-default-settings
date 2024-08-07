a2c3421a80389a62eeb3e512b6d518a8c13d2e64
Rahammetoela Toekiman <fusekai@outlook.com>
2024-08-07 10:33:00 -0300

    lockscreen random wallpaper

--- common/usr/share/sway/scripts/swaylock.sh
+++ common/usr/share/sway/scripts/swaylock.sh
@@ -2,16 +2,15 @@
 
 # This script takes a screenshot of current output and adds a blur and vignette to it
 
-cd /tmp
-grim screen.png
-
+cd $HOME/Pictures/wallpapers
+wallpaper=$(ls|sort -R|tail -1)
 # Delete existing image
-rm screen-out.png
-
+rm lockscreen.png
+ 
 #Adds a blur and vignette
-ffmpeg -i screen.png -vf "gblur=sigma=10, vignette=PI/5" -c:a copy screen-out.png
-
+ffmpeg -i $wallpaper -vf "gblur=sigma=10, vignette=PI/5" -c:a copy lockscreen.png
+ 
 #Uses output image with Swaylock
 swaylock \
---image screen-out.png \
+--image lockscreen.png \
 --daemonize
